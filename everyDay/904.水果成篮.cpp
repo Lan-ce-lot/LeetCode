747a79537ca99783ebbb4ab7419d8c02f6cc98a2
Lancel <lancel@LanceldeMacBook-Pro.local>
2022-10-17 19:05:30 +0800

    add 每日一天 2022/10/17

---
+++ everyDay/904.水果成篮.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=904 lang=cpp
+ *
+ * [904] 水果成篮
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int res = 1;
+        unordered_map<int, int> M;
+        int l = 0, r = 0;
+        for (; r < fruits.size(); r++) {
+            M[fruits[r]]++;
+            while (M.size() > 2) {
+                M[fruits[l]]--;
+                auto it = M.find(fruits[l]);
+                if (M[fruits[l]] == 0) {
+                    M.erase(it);
+                }
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
