4a279aef0dbf6df1eed940e0fd78b3f7bbc194a1
Lancelot <1984737645@qq.com>
2022-10-29 09:48:54 +0800

    add 1773 2022/10/29

---
+++ everyDay/1773.统计匹配检索规则的物品数量.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1773 lang=cpp
+ *
+ * [1773] 统计匹配检索规则的物品数量
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int n = items.size();
+        int f;
+        if (ruleKey == "type") {
+            f = 0;
+        } else if (ruleKey == "color") {
+            f = 1;
+        } else {
+            f = 2;
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (items[i][f] == ruleValue) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
